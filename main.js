48aa7a3bf691ac0bac2cb457870d596837e88a57
Axel <axelberdugo@gmail.com>
2016-07-08 12:47:43 +0100

    almost final css html structure, ill add score details later when codes come

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
   console.log("YO");
 
+var itemsContainer = document.getElementById("item-list");
+var items = ["http://www.fillmurray.com/50/50", "http://www.fillmurray.com/40/40", "cup"];
+var level = 0;
+var itemCount = 2;
+
+for (var i = 0; i < itemCount; i++) {
+  itemsContainer.innerHTML +=
+  '<div class="img50x50"><img src="'+ items[i] +'"></div>' +
+  '<div class="itemDescription">Description</div>';
+}
 
 
 
